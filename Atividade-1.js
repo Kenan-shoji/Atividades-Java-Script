e094372acb1c11e5861fcc48b1cf1783558bdc9c
kenanshoji <kenan_shoji@estudante.sc.senai.br>
2025-09-17 21:13:03 -0300

    atividade-1

---
+++ Atividade-1.js
@@ -0,0 +1,7 @@
+// Complete o código:
+let meuNome = "Kenan Shoji";
+let minhaIdade = 20;
+let minhaCidade = "Pomerode";
+
+// Exiba as informações usando template literals
+console.log(`Meu nome é ${meuNome}, tenho ${minhaIdade} anos e moro em ${minhaCidade}.`);
